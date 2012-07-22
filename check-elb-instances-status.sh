bb9867e0d72749659b5115e79ab7a613064a6f6a
Davi Alexandre <davi@davialexandre.com.br>
2012-07-22 00:51:14 -0300

    Adding validation for the number of command line arguments and usage help

--- check-elb-instances-status.sh
+++ check-elb-instances-status.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-for INSTANCE_ID in `elb-describe-instance-health $2 | cut -c14-23`
-do 
-	INSTANCE_IP=`ec2-describe-instances $INSTANCE_ID | sed -n 2p | cut -f17`
-	STATUS=`curl --resolve $1:80:$INSTANCE_IP $1 -I -s | head -n 1`
-	echo "$INSTANCE_ID: $STATUS"
-done
+if [ $# -eq 2 ]
+then
+
+	for INSTANCE_ID in `elb-describe-instance-health $2 | cut -c14-23`
+	do 
+		INSTANCE_IP=`ec2-describe-instances $INSTANCE_ID | sed -n 2p | cut -f17`
+		STATUS=`curl --resolve $1:80:$INSTANCE_IP $1 -I -s | head -n 1`
+		echo "$INSTANCE_ID: $STATUS"
+	done
+else
+	echo "USAGE: check-elb-instances-status.sh domain-name elb-name"
+fi
