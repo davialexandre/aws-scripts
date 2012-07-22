01f49ce6cd297ce09f1d508407a3bf4940c0ed60
Davi Alexandre <davi@davialexandre.com.br>
2012-07-22 00:42:35 -0300

    Creating script to check the status for every instance in a given elb

---
+++ check-elb-instances-status.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for INSTANCE_ID in `elb-describe-instance-health $2 | cut -c14-23`
+do 
+	INSTANCE_IP=`ec2-describe-instances $INSTANCE_ID | sed -n 2p | cut -f17`
+	STATUS=`curl --resolve $1:80:$INSTANCE_IP $1 -I -s | head -n 1`
+	echo "$INSTANCE_ID: $STATUS"
+done
